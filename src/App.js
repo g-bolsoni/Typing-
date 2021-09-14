9bfeef3a550e773438d0300c3163e0c9c5e88236
g-bolsoni <giovanebolsoni2@gmail.com>
2021-09-14 00:05:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const  App = () => {
+    return (
+        <div className='container'>
+            <div className="valid-keys">
+                <span className="matched">giov</span>
+                <span className="remainder">ane</span>
+            </div>
+            <div className="typed-keys">alofnaolfngiovdssg</div>
+            <div className="completed-words">
+                <ol>
+                    <li>cidade</li>
+                    <li>proffisional</li>
+                    <li>casa</li>
+                    <li>moradia</li>
+                    <li>lar</li>
+                </ol>
+            </div>
+        </div>
+    )
 }
-
 export default App;
