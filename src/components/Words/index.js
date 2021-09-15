38cdfcd4e0805e952aa7bc44218add129771242a
g-bolsoni <giovanebolsoni2@gmail.com>
2021-09-15 18:38:08 -0300

    Search the words and display typing

---
+++ src/components/Words/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Words({word, validKeys}) {
+    if(!word) return null;
+    const joinedKeys = validKeys.join('');
+    const matched = word.slice(0, joinedKeys.length);
+    const remainder = word.slice( joinedKeys.length)
+
+    return (
+        <>
+            <span className="matched">{matched}</span>
+            <span className="remainder">{remainder}</span>
+
+        </>
+    )
+}
